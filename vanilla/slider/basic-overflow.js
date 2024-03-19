67c5f5426247a0b1b81fb5ff685bdd854141d183
Philip YS * <wbvcos@gmail.com>
2024-03-20 06:01:32 +0900

    .

---
+++ vanilla/slider/basic-overflow.js
@@ -0,0 +1,150 @@
+!function(e){"function"==typeof define&&define.amd?define(e):e()}(function(){var e,t=["scroll","wheel","touchstart","touchmove","touchenter","touchend","touchleave","mouseout","mouseleave","mouseup","mousedown","mousemove","mouseenter","mousewheel","mouseover"];if(function(){var e=!1;try{var t=Object.defineProperty({},"passive",{get:function(){e=!0}});window.addEventListener("test",null,t),window.removeEventListener("test",null,t)}catch(e){}return e}()){var n=EventTarget.prototype.addEventListener;e=n,EventTarget.prototype.addEventListener=function(n,o,r){var i,s="object"==typeof r&&null!==r,u=s?r.capture:r;(r=s?function(e){var t=Object.getOwnPropertyDescriptor(e,"passive");return t&&!0!==t.writable&&void 0===t.set?Object.assign({},e):e}(r):{}).passive=void 0!==(i=r.passive)?i:-1!==t.indexOf(n)&&!0,r.capture=void 0!==u&&u,e.call(this,n,o,r)},EventTarget.prototype.addEventListener._original=e}});
+//# sourceMappingURL=index.umd.js.map
+
+
+window.Webflow?.push(function () {
+    window.innerWidth > 991 ? initializeBasicSlider() : $('.teaser-text-wrap').css('opacity', 1);
+
+    $(window).on('resize', function() {
+        window.innerWidth > 991 ? initializeBasicSlider() : $('.teaser-text-wrap').css('opacity', 1);
+    });
+
+    function initializeBasicSlider() {
+    let isRunning = false;
+    window.basicCurrentSlide = 1;
+    let basicSlideCount = $('.slider-img').length
+
+    initializeSlider()
+
+    function disableScrollWhileAnimation() {
+        document.body.style.overflow = 'hidden';
+    }
+
+    function enableScrollAfterAnimation() {
+        document.body.style.overflow = 'auto';
+    }
+
+    function findHigherAndLower(arr, num) {
+        const higher = [];
+        const lower = [];
+
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] > num) {
+                higher.push(arr[i]);
+            } else if (arr[i] < num) {
+                lower.push(arr[i]);
+            }
+        }
+        return { higher, lower };
+    }
+
+    function getLevel2Order () {
+        return $('.teaser-text-wrap').index($(`${window.location.hash}`))
+    }
+
+
+    function initializeSlider() {
+        window.basicCurrentSlide = (getLevel2Order() + 1) || 1
+
+        const slides = Array.from({ length: basicSlideCount }, (_, index) => index + 1)
+        const {higher, lower} = findHigherAndLower(slides, window.basicCurrentSlide);
+        higher.forEach(slide => {
+            $(`.slide-img-order-${slide}`).css('transform', 'translateY(100%)');
+        });
+        lower.forEach(slide => {
+            $(`.slide-img-order-${slide}`).css('transform', 'translateY(-100%)');
+        });
+
+        $(`.slide-img-order-${window.basicCurrentSlide}`).css('transform', 'translateY(0%)');
+
+        
+        $(`.text-wrap-${window.basicCurrentSlide}`).css('opacity', 1);
+        $(`.teaser-text-wrap:not(.text-wrap-${window.basicCurrentSlide})`).css('opacity', 0);
+    }
+
+    function runAnimation(direction) {
+        isRunning = true;
+        disableScrollWhileAnimation()
+        imageTransition(direction);
+        textTransition(direction);
+
+        setTimeout(() => {
+            isRunning = false;
+            enableScrollAfterAnimation()
+            window.basicCurrentSlide = direction === 'up' ? window.basicCurrentSlide - 1 : window.basicCurrentSlide + 1;
+        }, 1000);
+    }
+
+
+    document.querySelector('.basic-slider').addEventListener('wheel', function(e) {
+        if(window.innerWidth <= 991) {
+            return;    
+        }
+
+        const isSliderVisible = calculateVisibility(document.querySelector('.basic-slider')) === 0;
+        if(!isSliderVisible) return;
+
+        const direction = e.deltaY > 0 ? 'down' : 'up'
+        if(direction === 'up' && window.basicCurrentSlide === 1) return;
+        if(direction === 'down' && window.basicCurrentSlide === basicSlideCount) return;
+        
+
+
+        if (!isRunning) {
+            const elementB = document.querySelector('.intro-right-text');
+            const scrollHeight = elementB.scrollHeight;
+            const scrollTop = elementB.scrollTop;
+            const clientHeight = elementB.clientHeight;
+            if (e.deltaY > 0 && scrollTop + clientHeight >= scrollHeight - 1) {
+              runAnimation(direction);
+              document.querySelector('.intro-right-text').scrollTop = 0;
+              $('.intro-right-text').css('overflow', 'hidden');
+              setTimeout(() => {
+                $('.intro-right-text').css('overflow', 'auto');
+              }, 1000)
+            } else if (e.deltaY < 0 && scrollTop === 0) { // Check for scrolling up and reaching top
+                runAnimation(direction);
+                
+            }
+        }
+    });
+
+
+    function imageTransition(direction) {
+        const slidetoShow = direction === 'up' ? window.basicCurrentSlide - 1 : window.basicCurrentSlide + 1;
+        $(`.slide-img-order-${slidetoShow}`).css('transform', 'translateY(0%)');
+        
+        const moveTo = direction === 'up' ? 'translateY(100%)' : 'translateY(-100%)';
+        $(`.slide-img-order-${window.basicCurrentSlide}`).css('transform', moveTo);
+        // console.log("INTRansition", slidetoShow, window.basicCurrentSlide)
+    }
+
+    function textTransition(direction) {
+        const slidetoShow = direction === 'up' ? window.basicCurrentSlide - 1 : window.basicCurrentSlide + 1;
+        $(`.text-wrap-${slidetoShow}`).css('opacity', 1);
+        $(`.teaser-text-wrap:not(.text-wrap-${slidetoShow})`).css('opacity', 0);
+    }
+
+    function calculateVisibility(el) {
+        const rect = el.getBoundingClientRect();
+        const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+        const topVisible = rect.top >= 0 && rect.top < windowHeight;
+        const bottomVisible = rect.bottom > 0 && rect.bottom <= windowHeight;
+        const height = Math.min(rect.height, windowHeight);
+        let visibilityPercentage;
+        
+        if (topVisible && bottomVisible) {
+            visibilityPercentage = 100; // Fully visible
+        } else if (topVisible) {
+            visibilityPercentage = (rect.bottom / height) * 100; // Partially visible from top
+        } else if (bottomVisible) {
+            visibilityPercentage = ((windowHeight - rect.top) / height) * 100; // Partially visible from bottom
+        } else {
+            visibilityPercentage = 0; // Not visible
+        }
+        
+        return visibilityPercentage;
+    }
+}
+
+})
