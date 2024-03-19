52723fb0a70f0035806bd4c699443c775646a4c1
Philip YS * <wbvcos@gmail.com>
2024-03-19 23:28:10 +0900

    .

--- vanilla/slider/basic-v4.js
+++ vanilla/slider/basic-v4.js
@@ -1,14 +1,18 @@
+!function(e){"function"==typeof define&&define.amd?define(e):e()}(function(){var e,t=["scroll","wheel","touchstart","touchmove","touchenter","touchend","touchleave","mouseout","mouseleave","mouseup","mousedown","mousemove","mouseenter","mousewheel","mouseover"];if(function(){var e=!1;try{var t=Object.defineProperty({},"passive",{get:function(){e=!0}});window.addEventListener("test",null,t),window.removeEventListener("test",null,t)}catch(e){}return e}()){var n=EventTarget.prototype.addEventListener;e=n,EventTarget.prototype.addEventListener=function(n,o,r){var i,s="object"==typeof r&&null!==r,u=s?r.capture:r;(r=s?function(e){var t=Object.getOwnPropertyDescriptor(e,"passive");return t&&!0!==t.writable&&void 0===t.set?Object.assign({},e):e}(r):{}).passive=void 0!==(i=r.passive)?i:-1!==t.indexOf(n)&&!0,r.capture=void 0!==u&&u,e.call(this,n,o,r)},EventTarget.prototype.addEventListener._original=e}});
+//# sourceMappingURL=index.umd.js.map
+
 window.Webflow?.push(function () {
-    $(window).on('load resize', function() {
+    $(window).bind('load resize', function() {
+        console.log("load resize")
         if (window.innerWidth > 991) {
             initializeBasicSlider()
         } else {
             $('.teaser-text-wrap').css('opacity', 1);
         }
     });
-})
 
-function initializeBasicSlider() {
+
+    function initializeBasicSlider() {
     let isRunning = false;
     let basicCurrentSlide = 1;
     let basicSlideCount = $('.slider-img').length
@@ -78,6 +82,8 @@ function initializeBasicSlider() {
 
     document.querySelector('.basic-slider').addEventListener('wheel', function(e) {
         const isSliderVisible = calculateVisibility(document.querySelector('.basic-slider')) === 0;
+
+        console.log("isSliderVisible",isSliderVisible)
         if(!isSliderVisible) return;
 
         const direction = e.deltaY > 0 ? 'down' : 'up'
@@ -126,3 +132,5 @@ function initializeBasicSlider() {
         return visibilityPercentage;
     }
 }
+
+})
