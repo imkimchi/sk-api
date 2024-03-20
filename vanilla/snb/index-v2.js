db049ccf171e8e22129ebcfd6ae0a598319b9a7b
Philip YS * <wbvcos@gmail.com>
2024-03-20 18:20:27 +0900

    .

---
+++ vanilla/snb/index-v2.js
@@ -0,0 +1,178 @@
+var Webflow = Webflow || [];
+Webflow.push(function () {
+  
+const animationData = {"v":"5.12.2","fr":60,"ip":0,"op":87,"w":1000,"h":1000,"nm":"Tap","ddd":0,"assets":[],"layers":[{"ddd":0,"ind":1,"ty":4,"nm":"Main Dot","sr":1,"ks":{"o":{"a":0,"k":100,"ix":11},"r":{"a":0,"k":0,"ix":10},"p":{"a":0,"k":[500,500,0],"ix":2,"l":2},"a":{"a":0,"k":[200,132,0],"ix":1,"l":2},"s":{"a":1,"k":[{"i":{"x":[0.65,0.65,0.65],"y":[1,1,1]},"o":{"x":[0.35,0.35,0.35],"y":[0,0,0]},"t":0,"s":[100,100,100]},{"i":{"x":[0.65,0.65,0.65],"y":[1,1,1]},"o":{"x":[0.35,0.35,0.35],"y":[0,0,0]},"t":20,"s":[70,70,100]},{"t":40,"s":[100,100,100]}],"ix":6,"l":2}},"ao":0,"shapes":[{"ty":"gr","it":[{"d":1,"ty":"el","s":{"a":0,"k":[152,152],"ix":2},"p":{"a":0,"k":[0,0],"ix":3},"nm":"Ellipse Path 1","mn":"ADBE Vector Shape - Ellipse","hd":false},{"ty":"fl","c":{"a":0,"k":[0,0.321568627451,0.643137254902,1],"ix":4},"o":{"a":0,"k":100,"ix":5},"r":1,"bm":0,"nm":"Fill 1","mn":"ADBE Vector Graphic - Fill","hd":false},{"ty":"tr","p":{"a":0,"k":[200,132],"ix":2},"a":{"a":0,"k":[0,0],"ix":1},"s":{"a":0,"k":[100,100],"ix":3},"r":{"a":0,"k":0,"ix":6},"o":{"a":0,"k":100,"ix":7},"sk":{"a":0,"k":0,"ix":4},"sa":{"a":0,"k":0,"ix":5},"nm":"Transform"}],"nm":"Ellipse 1","np":3,"cix":2,"bm":0,"ix":1,"mn":"ADBE Vector Group","hd":false}],"ip":0,"op":88,"st":0,"ct":1,"bm":0},{"ddd":0,"ind":2,"ty":4,"nm":"Shadow dot 1","sr":1,"ks":{"o":{"a":1,"k":[{"i":{"x":[0.65],"y":[1]},"o":{"x":[0.35],"y":[0]},"t":6,"s":[0]},{"i":{"x":[0.672],"y":[2.087]},"o":{"x":[0.34],"y":[0]},"t":26,"s":[30]},{"i":{"x":[0.648],"y":[1]},"o":{"x":[0.339],"y":[0.213]},"t":29,"s":[30]},{"t":46,"s":[0]}],"ix":11},"r":{"a":0,"k":0,"ix":10},"p":{"a":0,"k":[500,500,0],"ix":2,"l":2},"a":{"a":0,"k":[200,132,0],"ix":1,"l":2},"s":{"a":1,"k":[{"i":{"x":[0.65,0.65,0.65],"y":[1,1,1]},"o":{"x":[0.35,0.35,0.35],"y":[0,0,0]},"t":6,"s":[180,180,100]},{"i":{"x":[0.65,0.65,0.65],"y":[1,1,1]},"o":{"x":[0.35,0.35,0.35],"y":[0,0,0]},"t":26,"s":[150,150,100]},{"t":46,"s":[180,180,100]}],"ix":6,"l":2}},"ao":0,"shapes":[{"ty":"gr","it":[{"d":1,"ty":"el","s":{"a":0,"k":[152,152],"ix":2},"p":{"a":0,"k":[0,0],"ix":3},"nm":"Ellipse Path 1","mn":"ADBE Vector Shape - Ellipse","hd":false},{"ty":"fl","c":{"a":0,"k":[0.164705882353,0.580392156863,1,1],"ix":4},"o":{"a":0,"k":100,"ix":5},"r":1,"bm":0,"nm":"Fill 1","mn":"ADBE Vector Graphic - Fill","hd":false},{"ty":"tr","p":{"a":0,"k":[200,132],"ix":2},"a":{"a":0,"k":[0,0],"ix":1},"s":{"a":0,"k":[100,100],"ix":3},"r":{"a":0,"k":0,"ix":6},"o":{"a":0,"k":100,"ix":7},"sk":{"a":0,"k":0,"ix":4},"sa":{"a":0,"k":0,"ix":5},"nm":"Transform"}],"nm":"Ellipse 1","np":3,"cix":2,"bm":0,"ix":1,"mn":"ADBE Vector Group","hd":false}],"ip":0,"op":88,"st":0,"ct":1,"bm":0},{"ddd":0,"ind":3,"ty":4,"nm":"Shadow dot 2","sr":1,"ks":{"o":{"a":1,"k":[{"i":{"x":[0.661],"y":[0.787]},"o":{"x":[0.352],"y":[0]},"t":12,"s":[0]},{"i":{"x":[0.66],"y":[1]},"o":{"x":[0.328],"y":[1.811]},"t":29,"s":[50]},{"i":{"x":[0.65],"y":[1]},"o":{"x":[0.35],"y":[0]},"t":32,"s":[50]},{"t":52,"s":[0]}],"ix":11},"r":{"a":0,"k":0,"ix":10},"p":{"a":0,"k":[500,500,0],"ix":2,"l":2},"a":{"a":0,"k":[200,132,0],"ix":1,"l":2},"s":{"a":1,"k":[{"i":{"x":[0.65,0.65,0.65],"y":[1,1,1]},"o":{"x":[0.35,0.35,0.35],"y":[0,0,0]},"t":12,"s":[250,250,100]},{"i":{"x":[0.65,0.65,0.65],"y":[1,1,1]},"o":{"x":[0.35,0.35,0.35],"y":[0,0,0]},"t":32,"s":[220,220,100]},{"t":52,"s":[250,250,100]}],"ix":6,"l":2}},"ao":0,"shapes":[{"ty":"gr","it":[{"d":1,"ty":"el","s":{"a":0,"k":[152,152],"ix":2},"p":{"a":0,"k":[0,0],"ix":3},"nm":"Ellipse Path 1","mn":"ADBE Vector Shape - Ellipse","hd":false},{"ty":"fl","c":{"a":0,"k":[0.682352941176,0.843137254902,1,1],"ix":4},"o":{"a":0,"k":100,"ix":5},"r":1,"bm":0,"nm":"Fill 1","mn":"ADBE Vector Graphic - Fill","hd":false},{"ty":"tr","p":{"a":0,"k":[200,132],"ix":2},"a":{"a":0,"k":[0,0],"ix":1},"s":{"a":0,"k":[100,100],"ix":3},"r":{"a":0,"k":0,"ix":6},"o":{"a":0,"k":100,"ix":7},"sk":{"a":0,"k":0,"ix":4},"sa":{"a":0,"k":0,"ix":5},"nm":"Transform"}],"nm":"Ellipse 1","np":3,"cix":2,"bm":0,"ix":1,"mn":"ADBE Vector Group","hd":false}],"ip":0,"op":88,"st":0,"ct":1,"bm":0}],"markers":[],"props":{}}
+const animationDataWhite = {"v":"5.12.2","fr":60,"ip":0,"op":87,"w":1000,"h":1000,"nm":"Tap","ddd":0,"assets":[{"id":"comp_0","nm":"Pre-comp 1","fr":60,"layers":[{"ddd":0,"ind":1,"ty":4,"nm":"Main Dot","sr":1,"ks":{"o":{"a":0,"k":100,"ix":11},"r":{"a":0,"k":0,"ix":10},"p":{"a":0,"k":[500,500,0],"ix":2,"l":2},"a":{"a":0,"k":[200,132,0],"ix":1,"l":2},"s":{"a":1,"k":[{"i":{"x":[0.65,0.65,0.65],"y":[1,1,1]},"o":{"x":[0.35,0.35,0.35],"y":[0,0,0]},"t":0,"s":[100,100,100]},{"i":{"x":[0.65,0.65,0.65],"y":[1,1,1]},"o":{"x":[0.35,0.35,0.35],"y":[0,0,0]},"t":20,"s":[70,70,100]},{"t":40,"s":[100,100,100]}],"ix":6,"l":2}},"ao":0,"shapes":[{"ty":"gr","it":[{"d":1,"ty":"el","s":{"a":0,"k":[152,152],"ix":2},"p":{"a":0,"k":[0,0],"ix":3},"nm":"Ellipse Path 1","mn":"ADBE Vector Shape - Ellipse","hd":false},{"ty":"fl","c":{"a":0,"k":[1,1,1,1],"ix":4},"o":{"a":0,"k":100,"ix":5},"r":1,"bm":0,"nm":"Fill 1","mn":"ADBE Vector Graphic - Fill","hd":false},{"ty":"tr","p":{"a":0,"k":[200,132],"ix":2},"a":{"a":0,"k":[0,0],"ix":1},"s":{"a":0,"k":[100,100],"ix":3},"r":{"a":0,"k":0,"ix":6},"o":{"a":0,"k":100,"ix":7},"sk":{"a":0,"k":0,"ix":4},"sa":{"a":0,"k":0,"ix":5},"nm":"Transform"}],"nm":"Ellipse 1","np":3,"cix":2,"bm":0,"ix":1,"mn":"ADBE Vector Group","hd":false}],"ip":0,"op":88,"st":0,"ct":1,"bm":0},{"ddd":0,"ind":2,"ty":4,"nm":"Shadow dot 1","sr":1,"ks":{"o":{"a":1,"k":[{"i":{"x":[0.65],"y":[1]},"o":{"x":[0.35],"y":[0]},"t":6,"s":[0]},{"i":{"x":[0.672],"y":[2.087]},"o":{"x":[0.34],"y":[0]},"t":26,"s":[30]},{"i":{"x":[0.648],"y":[1]},"o":{"x":[0.339],"y":[0.213]},"t":29,"s":[30]},{"t":46,"s":[0]}],"ix":11},"r":{"a":0,"k":0,"ix":10},"p":{"a":0,"k":[500,500,0],"ix":2,"l":2},"a":{"a":0,"k":[200,132,0],"ix":1,"l":2},"s":{"a":1,"k":[{"i":{"x":[0.65,0.65,0.65],"y":[1,1,1]},"o":{"x":[0.35,0.35,0.35],"y":[0,0,0]},"t":6,"s":[180,180,100]},{"i":{"x":[0.65,0.65,0.65],"y":[1,1,1]},"o":{"x":[0.35,0.35,0.35],"y":[0,0,0]},"t":26,"s":[150,150,100]},{"t":46,"s":[180,180,100]}],"ix":6,"l":2}},"ao":0,"shapes":[{"ty":"gr","it":[{"d":1,"ty":"el","s":{"a":0,"k":[152,152],"ix":2},"p":{"a":0,"k":[0,0],"ix":3},"nm":"Ellipse Path 1","mn":"ADBE Vector Shape - Ellipse","hd":false},{"ty":"fl","c":{"a":0,"k":[1,1,1,1],"ix":4},"o":{"a":0,"k":100,"ix":5},"r":1,"bm":0,"nm":"Fill 1","mn":"ADBE Vector Graphic - Fill","hd":false},{"ty":"tr","p":{"a":0,"k":[200,132],"ix":2},"a":{"a":0,"k":[0,0],"ix":1},"s":{"a":0,"k":[100,100],"ix":3},"r":{"a":0,"k":0,"ix":6},"o":{"a":0,"k":100,"ix":7},"sk":{"a":0,"k":0,"ix":4},"sa":{"a":0,"k":0,"ix":5},"nm":"Transform"}],"nm":"Ellipse 1","np":3,"cix":2,"bm":0,"ix":1,"mn":"ADBE Vector Group","hd":false}],"ip":0,"op":88,"st":0,"ct":1,"bm":0},{"ddd":0,"ind":3,"ty":4,"nm":"Shadow dot 2","sr":1,"ks":{"o":{"a":1,"k":[{"i":{"x":[0.661],"y":[0.787]},"o":{"x":[0.352],"y":[0]},"t":12,"s":[0]},{"i":{"x":[0.66],"y":[1]},"o":{"x":[0.328],"y":[1.811]},"t":29,"s":[50]},{"i":{"x":[0.65],"y":[1]},"o":{"x":[0.35],"y":[0]},"t":32,"s":[50]},{"t":52,"s":[0]}],"ix":11},"r":{"a":0,"k":0,"ix":10},"p":{"a":0,"k":[500,500,0],"ix":2,"l":2},"a":{"a":0,"k":[200,132,0],"ix":1,"l":2},"s":{"a":1,"k":[{"i":{"x":[0.65,0.65,0.65],"y":[1,1,1]},"o":{"x":[0.35,0.35,0.35],"y":[0,0,0]},"t":12,"s":[250,250,100]},{"i":{"x":[0.65,0.65,0.65],"y":[1,1,1]},"o":{"x":[0.35,0.35,0.35],"y":[0,0,0]},"t":32,"s":[220,220,100]},{"t":52,"s":[250,250,100]}],"ix":6,"l":2}},"ao":0,"shapes":[{"ty":"gr","it":[{"d":1,"ty":"el","s":{"a":0,"k":[152,152],"ix":2},"p":{"a":0,"k":[0,0],"ix":3},"nm":"Ellipse Path 1","mn":"ADBE Vector Shape - Ellipse","hd":false},{"ty":"fl","c":{"a":0,"k":[1,1,1,1],"ix":4},"o":{"a":0,"k":100,"ix":5},"r":1,"bm":0,"nm":"Fill 1","mn":"ADBE Vector Graphic - Fill","hd":false},{"ty":"tr","p":{"a":0,"k":[200,132],"ix":2},"a":{"a":0,"k":[0,0],"ix":1},"s":{"a":0,"k":[100,100],"ix":3},"r":{"a":0,"k":0,"ix":6},"o":{"a":0,"k":100,"ix":7},"sk":{"a":0,"k":0,"ix":4},"sa":{"a":0,"k":0,"ix":5},"nm":"Transform"}],"nm":"Ellipse 1","np":3,"cix":2,"bm":0,"ix":1,"mn":"ADBE Vector Group","hd":false}],"ip":0,"op":88,"st":0,"ct":1,"bm":0}]}],"layers":[{"ddd":0,"ind":1,"ty":0,"nm":"Pre-comp 2","refId":"comp_0","sr":1,"ks":{"o":{"a":0,"k":100,"ix":11},"r":{"a":0,"k":0,"ix":10},"p":{"a":0,"k":[500,500,0],"ix":2,"l":2},"a":{"a":0,"k":[500,500,0],"ix":1,"l":2},"s":{"a":0,"k":[100,100,100],"ix":6,"l":2}},"ao":0,"w":1000,"h":1000,"ip":0,"op":87,"st":0,"bm":0}],"markers":[],"props":{}}
+let animations = []
+let currentColor = 'blue'
+
+function initLottieAnimation(element, animationData) {
+  return lottie.loadAnimation({
+    container: element,
+    renderer: 'svg',
+    loop: true,
+    autoplay: false,
+    animationData: animationData
+  });
+}
+
+function stopAnimation(animation) {
+  animation.stop();
+}
+function stopOtherAnimations(currentAnimation) {
+  const allAnimations = document.querySelectorAll('.lottie');
+  allAnimations.forEach(animationElement => {
+    const animation = animationElement.lottie;
+    if (animation && animation !== currentAnimation) {
+      stopAnimation(animation);
+    }
+  });
+}
+
+
+const sellPointIcons = document.querySelectorAll('.sell-point-icon');
+
+sellPointIcons.forEach(iconElement => {
+  animations.push(initLottieAnimation(iconElement, animationData))
+});
+
+var sellPointLeftElement = document.querySelector('.sell-point-left');
+
+window.addEventListener('scroll', function() {
+      var scrollYValue = window.scrollY;
+    
+      if (scrollYValue > 918) {
+        sellPointLeftElement.style.position = 'fixed';
+        sellPointLeftElement.style.top = 'calc(50% - 160px)';
+      } else {
+        sellPointLeftElement.style.position = 'absolute';
+        sellPointLeftElement.style.top = '0';
+      }
+});
+
+
+function handleScreenWidth() {
+    var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+    if (screenWidth <= 1540) {
+        sellPointLeftElement.style.display = 'none';
+    } else {
+        sellPointLeftElement.style.display = 'flex'; // or 'inline', 'flex', etc. depending on your needs
+    }
+}
+
+
+handleScreenWidth();
+window.addEventListener('resize', handleScreenWidth);
+
+  const sections = Array.from({ length: 6 }, (_, n) => document.getElementById(`nav-section-${n + 1}`)).filter(Boolean);
+  const navLists = document.querySelectorAll(".sell-point-list");
+
+    function getVisiblePercentage(element) {
+        const rect = element.getBoundingClientRect();
+        const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+        const visibleHeight = Math.min(rect.bottom, windowHeight) - Math.max(rect.top, 0);
+        return (visibleHeight / element.clientHeight) * 100;
+    }
+
+    function getMostVisibleSection() {
+        let maxVisiblePercentage = 0;
+        let mostVisibleIndex = -1;
+
+        sections.forEach((section, index) => {
+            const visiblePercentage = getVisiblePercentage(section);
+
+            if (visiblePercentage > maxVisiblePercentage) {
+                maxVisiblePercentage = visiblePercentage;
+                mostVisibleIndex = index;
+            }
+        });
+
+        return mostVisibleIndex;
+    }
+
+    function setActiveNavLink(index) {
+      console.log("INDEX", index);
+
+      if (index === -1) {
+        // No section is visible, handle it accordingly
+        // For example, you might want to remove the "active" class from all links
+        navLists.forEach((link, i) => {
+          link.classList.remove("active");
+          animations[i].stop();
+        });
+      } else {
+        // At least one section is visible
+        navLists.forEach((link, i) => {
+          if (i === index) {
+            if (!link.classList.contains("active")) {
+              link.classList.add("active");
+              animations[i].play();
+              currentPlayingIndex = i
+              stopOtherAnimations(animations[i]);
+            }
+          } else {
+            link.classList.remove("active");
+            animations[i].stop();
+          }
+        });
+      }
+    }
+
+    function handleScroll() {
+        const mostVisibleIndex = getMostVisibleSection();
+        setActiveNavLink(mostVisibleIndex);
+
+        colorChangeHandler()
+    }
+
+    function colorChangeHandler() {
+      const sliderVisibility = isSliderVisible()
+      const colorToChange = sliderVisibility ? 'white' : 'blue';
+      const animationDatatoChange = sliderVisibility ? animationDataWhite : animationData;
+  
+      if(currentColor === colorToChange) return ;
+  
+      $('.text-block-4:not(.inner)').css('color', colorToChange === 'white' ? '#fff' : '#0052a4');
+  
+      animations = []
+      $('.sell-point-icon svg').remove()
+
+      sellPointIcons.forEach(iconElement => {
+          animations.push(initLottieAnimation(iconElement, animationDatatoChange))
+      });
+  
+      animations[currentPlayingIndex].play()
+  
+      currentColor = colorToChange
+  }
+
+  function isSliderVisible() {
+    var slider = $('.caps-speciality');
+    
+    if (slider.length === 0) {
+        console.error("Slider element not found.");
+        return false;
+    }
+    
+    var windowHeight = $(window).height();
+    var scrollTop = $(window).scrollTop();
+    var sliderTop = slider.offset().top;
+    var sliderHeight = slider.outerHeight();
+    var viewportTop = scrollTop;
+    var viewportBottom = scrollTop + windowHeight;
+    var sliderBottom = sliderTop + sliderHeight;
+    var visibleHeight = Math.min(viewportBottom, sliderBottom) - Math.max(viewportTop, sliderTop);
+    var visiblePercentage = (visibleHeight / sliderHeight) * 100;
+
+    if (visiblePercentage > 50) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+
+    window.addEventListener("scroll", handleScroll);
+});
